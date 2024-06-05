51618333d39e4dd30bea913f92bfaa655f6e21ba
Sacha Uijlen <sacha-u@hotmail.com>
2024-06-05 17:15:37 +0100

    html added via javascript

--- location.js
+++ location.js
@@ -16,36 +16,67 @@ function error(error) {
 }
 
 //go get the weather data from the api
-let result = "";
-
 async function getWeather(latitude, longitude) {
-  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=c9c04097e886eeff6e86ac740354f877`;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=c9c04097e886eeff6e86ac740354f877&units=metric`;
 
   //get data and turn into object
-  result = await fetch(url);
+  let result = await fetch(url);
   result = await result.json();
 
-  console.log(result.city, result.list);
+  // console.log(result.city, result.list);
+  // console.log(result);
+
+  // createLists("ul", result.list);
+  result.list.forEach((item) => {
+    create(item);
+  });
 }
 
 //turn it in html
-function createHTML(tag, text, container) {
+function createHTML(text, container, tag = "p") {
   let input = document.createTextNode(text);
   let element = document.createElement(tag);
   element.append(input);
   container.append(element);
 }
 
-createHTML("h1", "Weather", rootRef);
-createHTML("p", `here is the weather`, rootRef); // I tried to add the name of the city with ${result.city.name}, but that did not work. How would I do that?
+createHTML("Weather", rootRef, "h1");
+createHTML(`here is the weather`, rootRef, "p"); // I tried to add the name of the city with ${result.city.name}, but that did not work. How would I do that?
 
-function createLists(list, array) {
-  let element = document.createElement(list); // create ul or ol tag
-  let listItemsTags = document.createElement("li"); // create list items
-  let input = document.createTextNode("Item 1"); //create input list items
-  listItemsTags.append(input); // put input in list item
-  element.append(listItemsTags); // put list item in ul or ol
-  rootRef.append(element); // puts ul/ol in html
+function create(item) {
+  console.log(item);
+  //create div container
+  let div = document.createElement("div");
+  div.classList.add("weather-item");
+  rootRef.append(div);
+  //creating content
+  createHTML(Day(item.dt_txt), div, "h1");
+  createHTML(`Temperature: ${item.main.temp} °C`, div);
+  createHTML(`Maximum temperature: ${item.main.temp_max} °C`, div);
+  createHTML(`Minimum temperature: ${item.main.temp_min} °C`, div);
+  createHTML(`Windspeed: ${item.wind.speed}`, div);
+  createHTML(`Clouds: ${item.weather[0].description}`, div);
 }
 
-createLists("ul", result.list);
+function Day(date) {
+  const d = new Date(date);
+  let weekdayIndex = d.getDay();
+  console.log(weekdayIndex);
+  const week = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  date = date.replace("2024-", "");
+  date = date.substring(0, date.length - 3); // takes away the seconds
+  let dayTitle = week[weekdayIndex] + " " + date;
+  return dayTitle;
+}
+
+// const d = new Date("2024-06-05 18:00:00");
+// let day = d.getDay();
+// console.log(day);
