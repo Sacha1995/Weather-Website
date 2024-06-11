e4cf74f77401a4e4e6185c9185a23f4b2a7d5aa3
Sacha Uijlen <sacha-u@hotmail.com>
2024-06-11 19:44:54 +0100

    tried to make dropdown

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const rootRef = document.getElementById("root");
 let icon;
 let title;
 let intro;
+let display;
+let future;
+
+//capitalize first letter
+Object.defineProperty(String.prototype, "capitalize", {
+  value: function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  },
+  enumerable: false,
+});
 
 //initiate get location
 const options = { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 };
@@ -32,12 +42,11 @@ async function getWeather(latitude, longitude) {
 
   intro =
     result.city.name == undefined
-      ? `The weather for the nex 5 days;`
-      : `The weather for the nex 5 days in ${result.city.name}:`;
+      ? `The weather for the next 5 days;`
+      : `The weather for the next 5 days in ${result.city.name}:`;
 
   createHTML(intro, main, "intro", "p", "", "prepend");
   intro = document.querySelector(".intro");
-  console.log(intro);
   createHTML(title, main, "title", "h1", "", "prepend");
   title = document.querySelector(".title");
 
@@ -49,6 +58,15 @@ async function getWeather(latitude, longitude) {
   //filter
   result.list.forEach((item, index) => {
     filter(item, index);
+    display = document.getElementsByClassName("display");
+    future = document.getElementsByClassName("future");
+  });
+
+  let displayArr = Array.from(display);
+  let futureArr = Array.from(future);
+
+  displayArr.forEach((item) => {
+    comeDown(item, futureArr);
   });
 
   //answer and picture
@@ -91,7 +109,7 @@ function createHTML(
   element.classList.add(className);
 
   if (tag === "img") {
-    if (src.startsWith("./img")) {
+    if (src.startsWith(`./img`)) {
       element.setAttribute("src", src);
     } else {
       element.setAttribute("src", `./img/weather-icons/${src}.svg`);
@@ -107,21 +125,28 @@ function createHTML(
 
 function create(item, index) {
   //create div container - tried to use createHTML() did not work...
-  let div = document.createElement("div");
-  div.id = `weather-item${index}`;
-  containerWeather.append(div);
+  let weatherItem = document.createElement("div");
+  weatherItem.id = `weather-item${index}`;
+  containerWeather.append(weatherItem);
   //creating content
   const { dt, wind, weather } = item;
   const { temp_max, temp_min, temp } = item.main;
   icon = weather[0].icon;
 
-  createHTML(unixToHuman(dt), div, "date", "h2");
-  createHTML(`Temperature: ${Math.round(temp)} °C`, div, "temp");
-  createHTML(`Max temp: ${Math.round(temp_max)} °C`, div, "maxTemp");
-  createHTML(`Min temp: ${Math.round(temp_min)} °C`, div, "minTemp");
-  createHTML(`Windspeed: ${Math.round(wind.speed)}`, div, "wind");
-  createHTML(`Clouds: ${weather[0].description}`, div, "clouds");
-  createHTML("", div, "image", "img", icon);
+  createHTML(unixToHuman(dt), weatherItem, dt, "h2");
+  createHTML("", weatherItem, `statsContainer${index}`, "div");
+  // let date = document.querySelectorAll(".date")
+  let statsContainer = document.querySelector(`.statsContainer${index}`);
+  createHTML(`Temperature: ${Math.round(temp)} °C`, statsContainer, "temp");
+  createHTML(`Max temp: ${Math.round(temp_max)} °C`, statsContainer, "maxTemp");
+  createHTML(`Min temp: ${Math.round(temp_min)} °C`, statsContainer, "minTemp");
+  createHTML(`Windspeed: ${Math.round(wind.speed)}`, statsContainer, "wind");
+  createHTML(weather[0].description.capitalize(), statsContainer, "clouds");
+  createHTML("", weatherItem, `iconContainer${index}`, "div");
+  let iconContainer = document.querySelector(`.iconContainer${index}`);
+  iconContainer.classList.add("iconContainer");
+  createHTML(shortAnswer(item), iconContainer, "shortAnswer");
+  createHTML("", iconContainer, "image", "img", icon);
 }
 
 // get day and change date
@@ -187,6 +212,12 @@ function filter(item, index) {
     (date.getHours() === 12 || date.getHours() == 13)
   ) {
     weatherItem.classList.remove("off");
+    weatherItem.classList.add("display");
+  }
+  if (date.getDate() === today.getDate()) {
+    weatherItem.classList.add("today");
+  } else {
+    weatherItem.classList.add("future");
   }
 }
 
@@ -195,7 +226,6 @@ function answer(input) {
   let icon = input.weather[0].icon;
   let id = input.weather[0].id;
   let temp = input.main.temp;
-  console.log(id);
   if (icon[icon.length - 1] === "n") {
     return "No, it is dark outside.";
   } else if (icon === "11d") {
@@ -226,27 +256,95 @@ function picture(input) {
   let id = input.weather[0].id;
   let temp = input.main.temp;
   if (icon[icon.length - 1] === "n") {
-    console.log("you are getting here");
     return "./img/baby.png";
   } else if (icon === "11d") {
-    return "No, cuddle up";
+    return `./img/cuddle/${getRandom(1, 6)}.svg`;
   } else if (icon === "01d" || icon === "02d") {
-    if (temp > "18") {
-      return "Yes! Make sure to put sunscreen on";
-    } else if (temp > "27") {
-      return "Yes! Who has the waterballoons?";
+    if (temp > 18) {
+      return `./img/yes-warm/${getRandom(1, 7)}.svg`;
+    } else if (temp > 27) {
+      return `./img/yes-hot/${getRandom(1, 11)}.svg`;
     } else {
-      return "Yes! Don't forget your coat";
+      return `./img/yes/${getRandom(1, 23)}.svg`;
     }
-  } else if (id === "500" || id === "300" || id === "301") {
-    return "Yes, they are not made of sugar";
+  } else if (id === 500 || id === 300 || id === 301) {
+    return `./img/rain/${getRandom(1, 2)}.svg`;
   } else if (icon === "09d" || icon === "10d") {
-    return "No, where is that ipad again?";
-  } else if (id === "600") {
-    return "Yes, do you want to build a snowman?";
+    return `./img/no/${getRandom(1, 6)}.svg`;
+  } else if (id === 600) {
+    return `./img/snow-outside/${getRandom(1, 3)}.svg`;
   } else if (icon === "13d") {
-    return "No, let's make some hot coco";
+    return `./img/snow-inside/1.svg`;
   } else if (icon === "50d") {
-    return "Perfect weather for hide and seek";
+    return `./img/mist/1.svg`;
   }
 }
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function shortAnswer(item) {
+  let icon = item.weather[0].icon;
+  let id = item.weather[0].id;
+  if (icon[icon.length - 1] === "n") {
+    return "No";
+  } else if (id === 500 || id === 300 || id === 301) {
+    return "Yes";
+  } else if (
+    icon === "11d" ||
+    icon === "09d" ||
+    icon === "10d" ||
+    icon === "13d"
+  ) {
+    return "No";
+  } else if (icon === "50d") {
+    return "Probably not";
+  } else {
+    return "Yes";
+  }
+}
+
+//comedown
+function showItem() {
+  const item = document.querySelector(".future");
+  console.log("something");
+  item.toggleAttribute("hidden");
+}
+
+function comeDown(item, hiddenItem) {
+  item.addEventListener("click", (e) => {
+    hiddenItem.forEach((item2) => {
+      let unix = item.querySelector("h2").className;
+      let unix2 = item2.querySelector("h2").className;
+      let clickedDate = new Date(unix * 1000);
+      let hiddenDate = new Date(unix2 * 1000);
+      console.log(clickedDate);
+      console.log(hiddenDate);
+      if (hiddenDate.getDate() === clickedDate.getDate()) {
+        if (item2.classList.contains("off")) {
+          item2.classList.remove("off");
+          item2.classList.add("on");
+        } else if (item2.classList.contains(!"display")) {
+          item2.classList.remove("on");
+          item2.classList.add("off");
+        }
+      }
+    });
+  });
+}
+
+// displayArr.forEach((item) => {
+//   comeDown(item, result.list);
+// });
+
+// const animationIntervalGameHard = btnPlayHard.addEventListener(
+//   "click",
+//   (e) => {
+//     clearAnimation();
+//     turnOff();
+//     stopLight.classList.add("on");
+//     endTimer = setInterval(animationGameHard, 500);
+//     pedestrian.classList.remove("moveLeft");
+//   }
+// );
